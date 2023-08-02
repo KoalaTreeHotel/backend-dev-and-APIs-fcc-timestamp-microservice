f0df225510d070e25ab9c6bbef6a6ef1e2b7f2ea
KoalaTreeHotel <125790817+KoalaTreeHotel@users.noreply.github.com>
2023-08-02 11:28:52 +0100

    added api/:date endpoint, passing tests

--- index.js
+++ index.js
@@ -2,31 +2,50 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
+const daysArr = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const monthArr = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(__dirname + "/views/index.html");
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// api/:date endpoint
+app.get("/api/:date", function (req, res) {
+    console.log(req.params.date.toString());
+    const datedata = new Date(isNaN(req.params.date) ? req.params.date : req.params.date * 1);
+    if (datedata.getTime()) {
+        console.log(datedata);
+        const timeString = `${daysArr[datedata.getDay()]}, ${datedata.getDate() > 9 ? datedata.getDate() : `0${datedata.getDate()}`} ${monthArr[datedata.getMonth()]} ${datedata.getFullYear()} ${datedata.getHours() > 9 ? datedata.getHours() : `0${datedata.getHours()}`}:${datedata.getMinutes() > 9 ? datedata.getMinutes() : `0${datedata.getMinutes()}`}:${datedata.getSeconds() > 9 ? datedata.getSeconds() : `0${datedata.getSeconds()}`} GMT`;
+        res.json({ "unix": datedata.getTime(), "utc": timeString });
+    } else {
+        res.json({ error: "Invalid Date" });
+    }
+
 });
 
+// api endpoint
+app.get("/api", function (req, res) {
+    const datedata = new Date();
+    const timeString = `${daysArr[datedata.getDay()]}, ${datedata.getDate() > 9 ? datedata.getDate() : `0${datedata.getDate()}`} ${monthArr[datedata.getMonth()]} ${datedata.getFullYear()} ${datedata.getHours() > 9 ? datedata.getHours() : `0${datedata.getHours()}`}:${datedata.getMinutes() > 9 ? datedata.getMinutes() : `0${datedata.getMinutes()}`}:${datedata.getSeconds() > 9 ? datedata.getSeconds() : `0${datedata.getSeconds()}`} GMT`;
+    console.log(datedata);
+    res.json({ "unix": datedata.getTime(), "utc": timeString });
 
+});
 
-// listen for requests :)
+// listen for requests
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+    console.log("Your app is listening on port " + listener.address().port);
 });
